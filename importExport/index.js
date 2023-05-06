ebf292d3169d3e114a3c97f69e5b6b6c22fc4659
AneeshBist <aneeshbist@gmail.com>
2023-05-06 17:19:20 +0530

    custom modules creation and usage

---
+++ importExport/index.js
@@ -0,0 +1,12 @@
+const { add, sub, mul, div, name } = require("./operator.js");
+
+console.log(add(5, 5));
+console.log(sub(20, 5));
+console.log(mul(5, 5));
+console.log(div(20, 5));
+console.log(name);
+
+// const opr = require("./operator.js");
+
+// console.log(opr.add(5, 5));
+// console.log(opr.sub(20, 5));
---
+++ importExport/operator.js
@@ -0,0 +1,25 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const mul = (a, b) => {
+  return a * b;
+};
+
+const div = (a, b) => {
+  return a / b;
+};
+
+const name = "Beta";
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+// module.exports.multi = mul;
+// module.exports.divi = div;
+// module.exports.name = name;
+
+module.exports = { add, sub, mul, div, name };
