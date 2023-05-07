bd5a94efb312a84637a4ffccea06333b87f04f4c
AneeshBist <aneeshbist@gmail.com>
2023-05-07 12:49:24 +0530

    npm modules introduction

---
+++ npmMod/index.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+import validator from "validator";
+
+console.log(chalk.blue("Hello world!"));
+console.log(chalk.blue("Hello") + " World" + chalk.red("!"));
+console.log(chalk.red("Hello", chalk.underline.bgBlue("world") + "!"));
+
+const res1 = validator.isEmail("asdfgh@.gmail.com");
+console.log(res1 ? chalk.green.inverse(re1) : chalk.red.inverse(res1));
+
+const res2 = validator.isEmail("asdfgh@gmail.com");
+console.log(res2 ? chalk.green.inverse(res2) : chalk.red.inverse(res2));
