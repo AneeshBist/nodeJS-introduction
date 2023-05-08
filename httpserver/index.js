bf1f3ec12ace72d8c863fbef17a7b82ddf25752e
AneeshBist <aneeshbist@gmail.com>
2023-05-08 20:01:07 +0530

    url basics

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,7 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the other side");
+  //console.log(req.url);
+  if (req.url === "/") {
+    res.end("Hello from the other side");
+  } else if (req.url === "/about") {
+    res.end("Hello from the About us page");
+  } else if (req.url === "/contact") {
+    res.write("Hello from the Contact page");
+    res.end();
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("<h1>404 Page doest not exist</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
